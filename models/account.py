41713fb6474671ce952ad0d77a810da9849b1add
dansanti <dansanti@mail.com>
2017-04-17 03:44:40 -0300

    [IMP]Utilidades para obtener resumenes

--- models/account.py
+++ models/account.py
@@ -203,6 +203,40 @@ class account_move(models.Model):
     sended = fields.Boolean(string="Enviado al SII", default=False,readonly=True, states={'draft': [('readonly', False)]})
     factor_proporcionalidad = fields.Float(string="Factor proporcionalidad", default=0.00, readonly=True, states={'draft': [('readonly', False)]})
 
+    def _get_move_imps(self):
+        imps = {}
+        for l in self.line_ids:
+            if l.tax_line_id:
+                if l.tax_line_id:
+                    if not l.tax_line_id.id in imps:
+                        imps[l.tax_line_id.id] = {'tax_id':l.tax_line_id.id, 'credit':0 , 'debit': 0, 'code':l.tax_line_id.sii_code}
+                    imps[l.tax_line_id.id]['credit'] += l.credit
+                    imps[l.tax_line_id.id]['debit'] += l.debit
+                    if l.tax_line_id.activo_fijo:
+                        ActivoFijo[1] += l.credit
+            elif l.tax_ids and l.tax_ids[0].amount == 0: #caso monto exento
+                if not l.tax_ids[0].id in imps:
+                    imps[l.tax_ids[0].id] = {'tax_id':l.tax_ids[0].id, 'credit':0 , 'debit': 0, 'code':l.tax_ids[0].sii_code}
+                imps[l.tax_ids[0].id]['credit'] += l.credit
+                imps[l.tax_ids[0].id]['debit'] += l.debit
+        return imps
+
+    def totales_por_movimiento(self):
+        move_imps = self._get_move_imps()
+        imps = {'iva':0,
+                'exento':0,
+                'otros_imps':0,
+                }
+        for key, i in move_imps.items():
+            if i['code'] in [14]:
+                imps['iva']  += (i['credit'] or i['debit'])
+            elif i['code'] == 0:
+                imps['exento']  += (i['credit'] or i['debit'])
+            else:
+                imps['otros_imps']  += (i['credit'] or i['debit'])
+        imps['neto'] = self.amount - imps['otros_imps'] - imps['exento'] - imps['iva']
+        return imps
+
 
 class account_move_line(models.Model):
     _inherit = "account.move.line"
