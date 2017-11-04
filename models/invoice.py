cec7516d77c101f09ee6da60fe03b6f8a5a009a2
Nelson Ramírez Sánchez <info@nelsonramirez.com>
2017-11-04 13:36:51 -0300

    Remove old files

--- models/invoice.py
+++ models/invoice.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from odoo import osv, models, fields, api, _
-from odoo.osv import fields as old_fields
 from odoo.exceptions import except_orm, UserError
 import odoo.addons.decimal_precision as dp
 import logging
